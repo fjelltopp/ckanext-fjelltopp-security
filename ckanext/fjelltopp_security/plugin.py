babfe0aa0860a0026ba0738d1957580593c4582c
toavina <toavina@fjelltopp.org>
2025-01-30 14:02:15 +0300

    Added the OWASP headers middleware with tests

--- ckanext/fjelltopp_security/plugin.py
+++ ckanext/fjelltopp_security/plugin.py
@@ -4,6 +4,7 @@ import ckan.plugins.toolkit as toolkit
 
 class FjelltoppSecurityPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IMiddleware, inherit=True)
     
 
     # IConfigurer
@@ -13,4 +14,45 @@ class FjelltoppSecurityPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "fjelltopp_security")
 
-    
+
+    def make_middleware(self, app, config):
+        @app.after_request
+        def apply_owasp(response):
+            response.headers["Strict-Transport-Security"] = config.get(
+                "ckanext.fjelltopp_security.strict_transport_security",
+                "max-age=31536000; preload",
+            )
+            response.headers["X-Content-Type-Options"] = config.get(
+                "ckanext.fjelltopp_security.content_type_options", "nosniff"
+            )
+            response.headers["X-Permitted-Cross-Domain-Policies"] = config.get(
+                "ckanext.fjelltopp_security.cross_domain_policies",
+                "none",  # not sure about this one
+            )
+            response.headers["Referrer-Policy"] = config.get(
+                "ckanext.fjelltopp_security.referrer_policy",
+                "no-referrer-when-downgrade",  # this is default when not set
+            )
+            response.headers["Cache-Control"] = config.get(
+                "ckanext.fjelltopp_security.cache_control",
+                "",
+            )
+            response.headers["Cross-Origin-Opener-Policy"] = config.get(
+                "ckanext.fjelltopp_security.coop", "same-site"
+            )
+            response.headers["Cross-Origin-Embedder-Policy"] = config.get(
+                "ckanext.fjelltopp_security.coep", "unsafe-none"
+            )
+            response.headers["Cross-Origin-Resource-Policy"] = config.get(
+                "ckanext.fjelltopp_security.corp", "cross-origin"
+            )
+            response.headers["Content-Security-Policy"] = config.get(
+                "ckanext.fjelltopp_security.content_security_policy", ""
+            )
+            if ("Location" in response.headers) and (
+                "logged_out_redirect" in response.headers["Location"]
+            ):
+                response.headers["Clear-Site-Data"] = '"*"'
+            return response
+
+        return app
--- ckanext/fjelltopp_security/tests/test_plugin.py
+++ ckanext/fjelltopp_security/tests/test_plugin.py
@@ -1,56 +1,94 @@
-"""
-Tests for plugin.py.
+import pytest
+from ckan.tests import factories
+from ckan.plugins import toolkit
+from ckan.common import config
 
-Tests are written using the pytest library (https://docs.pytest.org), and you
-should read the testing guidelines in the CKAN docs:
-https://docs.ckan.org/en/2.9/contributing/testing.html
+@pytest.mark.ckan_config("ckan.plugins", "fjelltopp_security")
+@pytest.mark.usefixtures("clean_db", "with_plugins")
+class TestSecurityMiddleware:
 
-To write tests for your extension you should install the pytest-ckan package:
-
-    pip install pytest-ckan
-
-This will allow you to use CKAN specific fixtures on your tests.
-
-For instance, if your test involves database access you can use `clean_db` to
-reset the database:
-
-    import pytest
-
-    from ckan.tests import factories
-
-    @pytest.mark.usefixtures("clean_db")
-    def test_some_action():
-
+    def test_default_security_headers(self, app):
+        """Test that default security headers are set correctly"""
+        # Create a test dataset to get a valid URL
         dataset = factories.Dataset()
+        url = toolkit.url_for('dataset.read', id=dataset['name'])
+        
+        response = app.get(url)
+        
+        assert response.headers["Strict-Transport-Security"] == "max-age=31536000; preload"
+        assert response.headers["X-Content-Type-Options"] == "nosniff"
+        assert response.headers["X-Permitted-Cross-Domain-Policies"] == "none"
+        assert response.headers["Referrer-Policy"] == "no-referrer-when-downgrade"
+        assert response.headers["Cross-Origin-Opener-Policy"] == "same-site"
+        assert response.headers["Cross-Origin-Embedder-Policy"] == "unsafe-none"
+        assert response.headers["Cross-Origin-Resource-Policy"] == "cross-origin"
+        assert "Content-Security-Policy" in response.headers
+        assert "Cache-Control" in response.headers
 
-        # ...
 
-For functional tests that involve requests to the application, you can use the
-`app` fixture:
-
-    from ckan.plugins import toolkit
-
-    def test_some_endpoint(app):
-
-        url = toolkit.url_for('myblueprint.some_endpoint')
+    @pytest.mark.ckan_config("ckanext.fjelltopp_security.strict_transport_security", "max-age=86400")
+    @pytest.mark.ckan_config("ckanext.fjelltopp_security.content_type_options", "custom-value")
+    def test_custom_config_values(self, app):
+        """Test that custom config values override defaults"""
+        # Create a test dataset to get a valid URL
+        dataset = factories.Dataset()
+        url = toolkit.url_for('dataset.read', id=dataset['name'])
 
         response = app.get(url)
 
-        assert response.status_code == 200
+        assert response.headers["Strict-Transport-Security"] == "max-age=86400"
+        assert response.headers["X-Content-Type-Options"] == "custom-value"
 
+    def test_clear_site_data_on_logout(self, app):
+        """Test that Clear-Site-Data header is set on logout redirect"""
 
-To temporary patch the CKAN configuration for the duration of a test you can use:
+        # Create a mock route that simulates the logout redirect
+        @app.flask_app.route('/test_logout')
+        def test_logout():
+            response = app.flask_app.make_response('')
+            response.headers['Location'] = '/logged_out_redirect'
+            return response
 
-    import pytest
+        # Test the mock logout route
+        response = app.get('/test_logout')
 
-    @pytest.mark.ckan_config("ckanext.myext.some_key", "some_value")
-    def test_some_action():
-        pass
-"""
-import ckanext.fjelltopp_security.plugin as plugin
+        # Verify the Clear-Site-Data header is set
+        assert response.headers.get("Clear-Site-Data") == '"*"'
 
+    def test_clear_site_data_not_set_without_redirect(self, app):
+        """Test that Clear-Site-Data header is not set without logged_out_redirect"""
 
-@pytest.mark.ckan_config("ckan.plugins", "fjelltopp_security")
-@pytest.mark.usefixtures("with_plugins")
-def test_plugin():
-    assert plugin_loaded("fjelltopp_security")
+        # Create a mock route with a different redirect
+        @app.flask_app.route('/test_other_redirect')
+        def test_other_redirect():
+            response = app.flask_app.make_response('')
+            response.headers['Location'] = '/some_other_page'
+            return response
+
+        # Test the mock route
+        response = app.get('/test_other_redirect')
+
+        # Verify the Clear-Site-Data header is not set
+        assert "Clear-Site-Data" not in response.headers
+
+    def test_no_clear_site_data_on_normal_response(self, app):
+        """Test that Clear-Site-Data header is not set on normal responses"""
+        # Create a test dataset to get a valid URL
+        dataset = factories.Dataset()
+        url = toolkit.url_for('dataset.read', id=dataset['name'])
+
+        response = app.get(url)
+
+        assert "Clear-Site-Data" not in response.headers
+
+    @pytest.mark.ckan_config("ckanext.fjelltopp_security.content_security_policy",
+                             "default-src 'self'; script-src 'self' 'unsafe-inline'")
+    def test_content_security_policy(self, app):
+        """Test that Content-Security-Policy header is set correctly"""
+        dataset = factories.Dataset()
+        url = toolkit.url_for('dataset.read', id=dataset['name'])
+
+        response = app.get(url)
+
+        expected_csp = "default-src 'self'; script-src 'self' 'unsafe-inline'"
+        assert response.headers["Content-Security-Policy"] == expected_csp
