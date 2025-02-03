fdd6d77515b5be3f81e9f8dc9cd89a8edd76aaab
toavina <toavina@fjelltopp.org>
2025-02-03 08:50:17 +0300

    Fixing tests

--- ckanext/fjelltopp_security/tests/test_users.py
+++ ckanext/fjelltopp_security/tests/test_users.py
@@ -24,7 +24,6 @@ class TestSecureUserActions:
             'image_url': '/images/test-image.jpg'
         }
         user = factories.User(**user_dict)
-
         assert user['name'] == user_dict['name']
         assert user['email'] == user_dict['email']
         assert user['image_url'] == user_dict['image_url']
@@ -40,7 +39,6 @@ class TestSecureUserActions:
             factories.User(
                 image_url=image_url
             )
-
         assert 'Image URL must be a local path. External URLs are not allowed' in str(exception_info.value)
 
     def test_user_create_without_image_url(self):
@@ -87,17 +85,41 @@ class TestSecureUserActions:
             'password': 'APITestPass123',
             'image_url': 'https://example.com/image.jpg'
         }
-
         url = toolkit.url_for('api.action', ver=3, logic_function='user_create')
-
         response = app.post(
             url,
             json=user_dict,
             extra_environ=env,
             expect_errors=True
         )
-
         assert response.status_code == 409
         error_dict = response.json
         assert error_dict['success'] is False
         assert 'Image URL must be a local path' in str(error_dict['error'])
+
+
+    @pytest.mark.usefixtures('clean_db', 'with_plugins', 'with_request_context')
+    def test_api_user_create_with_valid_image(self, app):
+        """Test that the API allows local image paths during user creation."""
+        # Create sysadmin with a valid local image URL
+        sysadmin = factories.Sysadmin(
+            image_url='/images/default-user.png'
+        )
+        env = {'REMOTE_USER': sysadmin['name']}
+        user_dict = {
+            'name': 'apitestuser',
+            'email': 'apitest@example.com',
+            'password': 'APITestPass123',
+            'image_url': '/images/test-user.jpg'
+        }
+
+        url = toolkit.url_for('api.action', ver=3, logic_function='user_create')
+        response = app.post(
+            url,
+            json=user_dict,
+            extra_environ=env
+        )
+        assert response.status_code == 200
+        response_dict = response.json
+        assert response_dict['success'] is True
+        assert response_dict['result']['image_url'] == '/images/test-user.jpg'
