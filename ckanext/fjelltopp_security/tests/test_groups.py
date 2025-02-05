71bc8d41e11d3cf6a52392f251d2bfcada757647
toavina <toavina@fjelltopp.org>
2025-02-05 12:30:00 +0300

    Updated group tests

--- ckanext/fjelltopp_security/tests/test_groups.py
+++ ckanext/fjelltopp_security/tests/test_groups.py
@@ -1,177 +1,85 @@
 import pytest
 from ckan.tests import factories
-from ckan.plugins import toolkit
 from ckan.logic import ValidationError
+from ckan.tests.helpers import call_action
 
 
 @pytest.mark.ckan_config("ckan.plugins", "fjelltopp_security")
 @pytest.mark.usefixtures("clean_db", "with_plugins")
 class TestSecureGroupActions:
 
-    @pytest.fixture
-    def base_group(self):
-        """Create a basic group with factories."""
-        return factories.Group(
-            image_url='/images/test-group-image.jpg'
-        )
+    def test_group_create_with_valid_data(self, context):
 
-    def test_group_create_with_valid_data(self):
-        """Test group creation with valid local image URL succeeds."""
         group_dict = {
             'name': 'testgroup',
             'title': 'Test Group',
             'image_url': '/images/test-group-image.jpg'
         }
-        group = factories.Group(**group_dict)
-
-        assert group['name'] == group_dict['name']
-        assert group['title'] == group_dict['title']
+        group = call_action('group_create', context=context, **group_dict)
         assert group['image_url'] == group_dict['image_url']
+
 
     @pytest.mark.parametrize("image_url", [
         'http://example.com/group-image.jpg',
         'https://example.com/group-image.jpg',
         '//example.com/group-image.jpg'
     ])
-    def test_group_create_with_external_image_fails(self, image_url):
-        """Test group creation fails with external image URLs."""
+    def test_group_create_with_external_image_fails(self, image_url, context):
         with pytest.raises(ValidationError) as exception_info:
-            factories.Group(
+            call_action(
+                'group_create',
+                name='testgroup',
+                title='Test Group',
+                image_url=image_url,
+                context=context
+            )
+
+
+    def test_group_create_without_image_url(self, context):
+        group = call_action(
+            'group_create',
+            context=context,
+            name='testgroup',
+            title='Test Group',
+            image_url=None
+        )
+        assert 'image_url' not in group or not group['image_url']
+
+
+    def test_group_update_with_valid_data(self, group):
+        update_dict = {
+            'id': group['id'],
+            'name': group['name'],
+            'title': 'Updated Test Group',
+            'image_url': '/images/updated-group-image.jpg',
+        }
+        updated_group = call_action('group_update', **update_dict)
+        assert updated_group['image_url'] == update_dict['image_url']
+
+
+    def test_group_update_without_image_url(self, group):
+        original_image_url = group['image_url']
+        update_dict = {
+            'id': group['id'],
+            'name': group['name'],
+            'title': 'Updated Test Group'
+        }
+        updated_group = call_action('group_update', **update_dict)
+        assert updated_group['image_url'] == original_image_url
+
+
+    @pytest.mark.parametrize("image_url", [
+        'http://example.com/group-image.jpg',
+        'https://example.com/group-image.jpg',
+        '//example.com/group-image.jpg'
+    ])
+    def test_group_update_with_external_image_fails(self, group, image_url):
+        with pytest.raises(ValidationError) as exception_info:
+            call_action(
+                'group_update',
+                id=group['id'],
+                name=group['name'],
+                title='Updated Test Group',
                 image_url=image_url
             )
 
-        assert 'Image URL must be a local path. External URLs are not allowed' in str(exception_info.value)
-
-    def test_group_create_without_image_url(self):
-        """Test group creation succeeds without an image URL."""
-        group = factories.Group(image_url=None)
-        assert 'image_url' not in group or not group['image_url']
-
-    def test_group_update_with_valid_data(self, base_group):
-        """Test group update with valid local image URL succeeds."""
-        update_dict = {
-            'id': base_group['id'],
-            'name': 'testgroup',
-            'title': 'Updated Test Group',
-            'image_url': '/images/updated-group-image.jpg'
-        }
-        updated_group = toolkit.get_action('group_update')(
-            context={'ignore_auth': True},
-            data_dict=update_dict
-        )
-        assert updated_group['image_url'] == update_dict['image_url']
-        assert updated_group['title'] == update_dict['title']
-
-    def test_group_update_without_image_url(self, base_group):
-        """Test group update succeeds when not modifying image URL."""
-        original_image_url = base_group['image_url']
-        update_dict = {
-            'id': base_group['id'],
-            'name': 'testgroup',
-            'title': 'Updated Test Group'
-        }
-        updated_group = toolkit.get_action('group_update')(
-            context={'ignore_auth': True},
-            data_dict=update_dict
-        )
-        assert updated_group['image_url'] == original_image_url
-
-    @pytest.mark.parametrize("image_url", [
-        'http://example.com/group-image.jpg',
-        'https://example.com/group-image.jpg',
-        '//example.com/group-image.jpg'
-    ])
-    def test_group_update_with_external_image_fails(self, base_group, image_url):
-        """Test group update fails with external image URLs."""
-        update_dict = {
-            'id': base_group['id'],
-            'name': 'testgroup',
-            'title': 'Updated Test Group',
-            'image_url': image_url
-        }
-        with pytest.raises(ValidationError) as exception_info:
-            toolkit.get_action('group_update')(
-                context={'ignore_auth': True},
-                data_dict=update_dict
-            )
-
-        assert 'Image URL must be a local path. External URLs are not allowed' in str(exception_info.value)
-
-
-    # Web API
-    @pytest.mark.usefixtures('clean_db', 'with_plugins', 'with_request_context')
-    class TestSecureGroupAPI:
-
-        def test_api_group_create_with_external_image(self, _call_api, _assert):
-            """Test that the API blocks external images during group creation."""
-            group_dict = {
-                'name': 'test-group',
-                'title': 'Test Group',
-                'image_url': 'https://example.com/image.jpg'
-            }
-            _assert(
-                _call_api(group_dict, 'group_create', True),
-                False
-            )
-
-        def test_api_group_update_with_external_image(self, _call_api, _assert):
-            """Test that the API blocks external images during group update."""
-            group = factories.Group(image_url='/images/original.jpg')
-            update_dict = {
-                'id': group['id'],
-                'image_url': 'https://example.com/updated-image.jpg'
-            }
-            _assert(
-                _call_api(update_dict, 'group_update', True),
-                False
-            )
-
-        def test_api_group_update_with_valid_image(self, _call_api, _assert):
-            """Test that the API allows local images during group update."""
-            group = factories.Group(image_url='/images/original.jpg')
-            update_dict = {
-                'id': group['id'],
-                'image_url': '/images/updated-image.jpg'
-            }
-            _assert(
-                _call_api(update_dict, 'group_update'),
-                True,
-                '/images/updated-image.jpg'
-            )
-
-        def test_api_group_create_with_valid_image(self, _call_api, _assert):
-            """Test that the API allows local images during group creation."""
-            group_dict = {
-                'name': 'test-group',
-                'title': 'Test Group',
-                'image_url': '/images/test-image.jpg'
-            }
-            _assert(
-                _call_api(group_dict, 'group_create'),
-                True
-            )
-
-        def test_api_group_create_without_image(self, _call_api, _assert):
-            """Test that the API allows group creation without an image URL."""
-            group_dict = {
-                'name': 'test-group',
-                'title': 'Test Group'
-            }
-            _assert(
-                _call_api(group_dict, 'group_create'),
-                True,
-                ''
-            )
-
-        def test_api_group_update_remove_image(self, _call_api, _assert):
-            """Test that the API allows removing image URL during update."""
-            group = factories.Group(image_url='/images/original.jpg')
-            update_dict = {
-                'id': group['id'],
-                'image_url': ''
-            }
-            _assert(
-                _call_api(update_dict, 'group_update'),
-                True,
-                ''
-            )
